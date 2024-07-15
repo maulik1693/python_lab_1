c16650eeb86434e7f541749b54504b1fa3cf67cb
maulik1693 <bca2022maulik1693@tnraocollege.org>
2024-07-15 13:55:37 +0530

    Add files via upload

---
+++ Maulik/emp.py
@@ -0,0 +1,13 @@
+EMPNAME = []
+
+EMPNAME. append("Raj")
+EMPNAME. append("Maulik")
+EMPNAME.append("Kishor")
+
+print("Employee Names after additions:", EMPNAME)
+EMPNAME.remove("Raj")
+
+print("Employee Names after removal  :", EMPNAME)
+EMPNAME. append("Ashok")
+
+print("Final Employee Names          :", EMPNAME)
